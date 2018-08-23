f4cf930e20b7959b0f6c5efde5eae4a01d66efd8
djangulo <denis.angulo@gmail.com>
2018-08-23 19:39:23 -0400

    symbolic deletion of old named dir

--- recruitment/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-from .celery_app import app as celery_app
-__all__ = ['celery_app']
--- recruitment/context_processors.py
+++
@@ -1,9 +0,0 @@
-"""Brand context processor (for all brand variables)"""
-
-from django.conf import settings
-
-def get_brand_dict(request):
-    return settings.BRAND_DICT
-
-def get_brand_bool(request):
-    return {'BRANDING': settings.BRANDING}
