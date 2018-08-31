ce7fe0cc12af505d686c623c2edcaa9801ec8167
djangulo <denis.angulo@gmail.com>
2018-08-31 12:10:28 -0400

    extended signals to create profile and primary email on user creation, and tested

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,11 +3,21 @@ from django.core.exceptions import ValidationError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from accounts.models import User, Profile
+from accounts.models import User, Profile, EmailAddress
 
 @receiver(post_save, sender=User)
 #pylint: disable=W0613
-def create_profile_for_user(sender, instance, **kwargs):
+def create_profile_for_user(sender, instance, created, **kwargs):
     """Create and assign a Profile object after a User object is
     created."""
-    Profile.objects.get_or_create(user=instance)
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def create_email_for_user(sender, instance, created, **kwargs):
+    """Create and assign an object after a User object is
+    created."""
+    if created:
+        EmailAddress.objects.create(email=instance.email,
+                                    is_primary=True,
+                                    user=instance)
--- accounts/tests/test_signals.py
+++ accounts/tests/test_signals.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from accounts.models import Profile, User
+from accounts.models import Profile, User, EmailAddress
 
 from .test_models import USERNAME, PASSWORD, EMAIL
 
@@ -10,3 +10,26 @@ class AccountSignalTest(TestCase):
         user = User.objects.create_user(email=EMAIL, password=PASSWORD,
                                         username=USERNAME)
         self.assertEqual(user.profile, Profile.objects.first())
+
+    def test_profile_reverse_relation(self):
+        user = User.objects.create_user(email=EMAIL, password=PASSWORD,
+                                        username=USERNAME)
+        profile = Profile.objects.first()
+        self.assertEqual(profile.user, user)
+
+    def test_create_user_triggers_email_creation(self):
+        user = User.objects.create_user(email=EMAIL, password=PASSWORD,
+                                        username=USERNAME)
+        self.assertEqual(user.email, EmailAddress.objects.first().email)
+
+    def test_email_reverse_relation(self):
+        user = User.objects.create_user(email=EMAIL, password=PASSWORD,
+                                        username=USERNAME)
+        email = EmailAddress.objects.first()
+        self.assertEqual(email.user, user)
+
+    def test_email_reverse_relation_accessor(self):
+        user = User.objects.create_user(email=EMAIL, password=PASSWORD,
+                                        username=USERNAME)
+        email = EmailAddress.objects.first()
+        self.assertIn(email, user.email_addresses.all())
